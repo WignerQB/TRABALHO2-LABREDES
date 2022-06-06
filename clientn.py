a67c984dc7761acdc7069d7cafa9a6382e41a29f
WignerQB <josewqb@gmail.com>
2022-06-06 14:25:21 +0000

    V7 Cliente recebe mensagem

--- clientn.py
+++ clientn.py
@@ -10,11 +10,22 @@ try:
 except socket.error as e:
     print(str(e))
 
+print("As mensagens devem conter o destinatário no início e a mensagem logo após, com um $ separando")
+print("Exemplo: C1$Ola Mundo!")
+
 Response = ClientSocket.recv(1024)
 while True:
-    Input = input('Say Something: ')
-    ClientSocket.send(str.encode(Input))
-    Response = ClientSocket.recv(1024)
-    print(Response.decode('utf-8'))
+    MSG = input('Eu: ')
+    VETORMSG = MSG.split("$")
+    try:
+        ind = VETORMSG[0].index("G10")
+    except:
+        ind = -1
+    if ind ==  -1 and len(VETORMSG) > 1:
+        ClientSocket.send(str.encode(MSG))
+        Response = ClientSocket.recv(1024)
+        print(Response.decode('utf-8'))
+    else:
+        print("Mensagem inválida!")
 
 ClientSocket.close()
